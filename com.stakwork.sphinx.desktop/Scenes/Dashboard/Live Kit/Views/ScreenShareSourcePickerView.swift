c12a384b38b88b87fbec504fcacbd5abfc8f50e4
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2025-01-13 10:28:00 -0300

    Fix for screen sharing not including current application

--- com.stakwork.sphinx.desktop/Scenes/Dashboard/Live Kit/Views/ScreenShareSourcePickerView.swift
+++ com.stakwork.sphinx.desktop/Scenes/Dashboard/Live Kit/Views/ScreenShareSourcePickerView.swift
@@ -42,7 +42,7 @@ class ScreenShareSourcePickerCtrl: ObservableObject {
             }
         }
 
-        let sources = try await MacOSScreenCapturer.sources(for: mode == .display ? .display : .window)
+        let sources = try await MacOSScreenCapturer.sources(for: mode == .display ? .display : .window, includeCurrentApplication: true)
         let options = ScreenShareCaptureOptions(dimensions: .h360_43, fps: 5, includeCurrentApplication: true)
         let _newTracks = sources.map { LocalVideoTrack.createMacOSScreenShareTrack(source: $0, options: options) }
 
