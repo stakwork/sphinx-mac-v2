f31728f36864a4768dc2794f1e154e4ce2d2e764
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2026-04-05 23:06:15 -0300

    UI fix

--- com.stakwork.sphinx.desktop/Scenes/Profile/View Controllers/ProfileViewController.swift
+++ com.stakwork.sphinx.desktop/Scenes/Profile/View Controllers/ProfileViewController.swift
@@ -429,7 +429,7 @@ class ProfileViewController: NSViewController {
             vc: setupAIAgentVC,
             identifier: "setup-ai-agent-window",
             title: "Configure AI Agent",
-            height: 580
+            height: 490
         )
     }
     
