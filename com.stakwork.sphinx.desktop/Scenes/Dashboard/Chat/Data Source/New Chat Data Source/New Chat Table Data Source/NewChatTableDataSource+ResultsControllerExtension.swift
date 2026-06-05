374541cb34512f06413a6e61e93e3ddec765c04e
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2026-06-05 13:50:37 -0300

    Fixes for loading more items row

--- com.stakwork.sphinx.desktop/Scenes/Dashboard/Chat/Data Source/New Chat Data Source/New Chat Table Data Source/NewChatTableDataSource+ResultsControllerExtension.swift
+++ com.stakwork.sphinx.desktop/Scenes/Dashboard/Chat/Data Source/New Chat Data Source/New Chat Table Data Source/NewChatTableDataSource+ResultsControllerExtension.swift
@@ -374,7 +374,7 @@ extension NewChatTableDataSource {
         processMessages(
             messages: messagesArray,
             UIUpdateIndex: self.UIUpdateIndex,
-            showLoadingMore: !allItemsLoaded
+            showLoadingMore: !allItemsLoaded && self.chat?.conversationContact?.isAgent != true
         )
     }
     
@@ -874,7 +874,7 @@ extension NewChatTableDataSource : @preconcurrency NSFetchedResultsControllerDel
                     self.processMessages(
                         messages: self.messagesArray,
                         UIUpdateIndex: self.UIUpdateIndex,
-                        showLoadingMore: !self.allItemsLoaded
+                        showLoadingMore: !self.allItemsLoaded && self.chat?.conversationContact?.isAgent != true
                     )
                     self.configureSecondaryMessagesResultsController()
                     
@@ -890,7 +890,7 @@ extension NewChatTableDataSource : @preconcurrency NSFetchedResultsControllerDel
                 self.processMessages(
                     messages: self.messagesArray,
                     UIUpdateIndex: self.UIUpdateIndex,
-                    showLoadingMore: !self.allItemsLoaded
+                    showLoadingMore: !self.allItemsLoaded && self.chat?.conversationContact?.isAgent != true
                 )
             }
             
