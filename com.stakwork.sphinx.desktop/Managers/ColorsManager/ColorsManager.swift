e8da2191e5e90f19dbdf0e1178582aaefc482195
tomastiminskas <tomastiminskas@users.noreply.github.com>
2026-09-14 15:51:57 +0000

    Generated with Hive: Fix SIGABRT by copying Cocoa dictionaries before mutate or iterate

--- com.stakwork.sphinx.desktop/Managers/ColorsManager/ColorsManager.swift
+++ com.stakwork.sphinx.desktop/Managers/ColorsManager/ColorsManager.swift
@@ -21,7 +21,11 @@ class ColorsManager : NSObject, @unchecked Sendable {
     
     func storeColorsInMemory() {
         let userDefaults = UserDefaults.standard
-        let allDefaults = userDefaults.dictionaryRepresentation()
+        let allDefaults = userDefaults.dictionaryRepresentation().reduce(
+            into: [String: Any]()
+        ) { result, pair in
+            result[pair.key] = pair.value
+        }
 
         for (key, value) in allDefaults {
             if key.contains("-color"), let value = value as? String {
